ec53d5335581a85df3bc9d151edd1ff95be6bb27
sulecelep <ssulecelepp@gmail.com>
2023-01-11 17:13:40 +0300

    Kullanıcı Profilinde Güncellemeler yapıldı

---
+++ eCommerceProject/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceProject.Controllers
+{
+    public class ProfileController : Controller
+    {
+        TraderManager tm = new TraderManager(new EfTraderRepository());
+
+        public IActionResult profilim()
+        {
+            var values = tm.TGetList();
+            return View(values);
+        }
+    }
+}
--- eCommerceProject/Controllers/TraderController.cs
+++ eCommerceProject/Controllers/TraderController.cs
@@ -7,7 +7,17 @@ namespace eCommerceProject.Controllers
     public class TraderController : Controller
     {
         ProductManager mc = new ProductManager(new EfProductRepository());
-        public IActionResult Index()
+        public IActionResult ilanlarim()
+        {
+            var values = mc.TGetList();
+            return View(values);
+        }
+        public IActionResult aldiklarim()
+        {
+            var values = mc.TGetList();
+            return View(values);
+        }
+        public IActionResult sattiklarim()
         {
             var values = mc.TGetList();
             return View(values);
